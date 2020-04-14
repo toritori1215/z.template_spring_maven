bd73e065aa6fa9a6f1df87ea95713d5c735ccd7f
IHB <IHB@192.168.1.27>
2020-04-14 18:44:13 +0900

    ihb
    Signed-off-by: IHB <IHB@192.168.1.27>

--- z.template_spring_maven/src/main/java/com/itwill/hotel/controller/ProductController.java
+++ z.template_spring_maven/src/main/java/com/itwill/hotel/controller/ProductController.java
@@ -3,9 +3,6 @@ package com.itwill.hotel.controller;
 import java.util.HashMap;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
