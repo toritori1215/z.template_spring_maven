2af0498399b7859f6aacf036bd678f4f146e1356
IHB <IHB@192.168.1.27>
2020-04-23 17:38:05 +0900

    ihb
    Signed-off-by: IHB <IHB@192.168.1.27>

--- z.template_spring_maven/src/main/java/com/itwill/hotel/controller/JumunController.java
+++ z.template_spring_maven/src/main/java/com/itwill/hotel/controller/JumunController.java
@@ -2,11 +2,14 @@ package com.itwill.hotel.controller;
 
 import java.util.HashMap;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.itwill.hotel.domain.Member;
 import com.itwill.hotel.service.JumunService;
 
 @Controller
@@ -16,8 +19,13 @@ public class JumunController {
 	JumunService jumunService;
 	
 	@RequestMapping(value = "/cart_checkout")
-	public String cartPayment() {
-		return "common_payment_fixed_sidebar";
+	public String cartPayment(HttpSession session) {
+		Member member = (Member) session.getAttribute("sUser");
+		if (member == null) {
+			return "redirect:/member_login_form";
+		} else {
+			return "common_payment_fixed_sidebar";
+		}
 	}
 	
 	@RequestMapping(value = "/checkout_payment")
