82710d7fd119e6791e2083ced801b08b92c166dc
IHB <IHB@192.168.1.27>
2020-04-24 09:48:13 +0900

    ihb
    Signed-off-by: IHB <IHB@192.168.1.27>

--- z.template_spring_maven/src/main/java/com/itwill/hotel/repository/JumunDao.java
+++ z.template_spring_maven/src/main/java/com/itwill/hotel/repository/JumunDao.java
@@ -4,13 +4,18 @@ import java.util.HashMap;
 import java.util.List;
 
 import com.itwill.hotel.domain.Jumun;
+import com.itwill.hotel.domain.JumunDetail;
 
 public interface JumunDao {
 	
 	public List<Jumun> selectJumun(int mNo);
 	
+	public int selectRecentJumun(int mNo);
+	
 	public int insertJumun(Jumun jumun);
 	
 	public int cancelJumun(HashMap hashMap);
+	
+	public int insertJumunDetail(JumunDetail jumunDetail);
 	
 }
